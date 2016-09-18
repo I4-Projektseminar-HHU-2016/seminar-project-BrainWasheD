2fedd9383e95d828ec4f9a2f4364502c27ba6051
Raphael Katschke <Raphael.Katschke@uni-duesseldorf.de>
2016-09-18 07:02:48 +0200

    +twitter_keys example

---
+++ twitter_keys.py
@@ -0,0 +1,7 @@
+# Consumer keys and access tokens, used for OAuth
+# https://apps.twitter.com
+# Erstelle Dir eine App und verwende die Keys hier
+consumer_key = 'consumer key'
+consumer_secret = 'consumer secret'
+access_token = 'access token'
+access_token_secret = 'access token secret'
